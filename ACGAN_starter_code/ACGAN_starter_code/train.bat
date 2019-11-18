4f1917312e8956bdcfe1607e8b1edd526e7125bc
Khuyen Duong <truckhuyen@gmai.com>
2019-11-18 18:34:10 +0000

    Updated train

--- ACGAN_starter_code/ACGAN_starter_code/train.bat
+++ ACGAN_starter_code/ACGAN_starter_code/train.bat
@@ -1,1 +1,1 @@
-python main.py --dataset=cifar10 --dataroot=datasets --batchSize=4 --ngpu=0 --netG=./netG_epoch_100.pth --outf=outputs
+python main.py --dataset=cifar10 --dataroot=datasets --batchSize=4 --ngpu=1 --netG=./netG_epoch_100.pth --outf=outputs
